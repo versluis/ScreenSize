f9fe028f9761eecb252073ae4abeadeff7220416
Jay Versluis <versluis@Jays-MacBook-Pro.local>
2013-11-06 04:01:46 -0500

    Initial Commit

---
+++ ScreenSize/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  ScreenSize
+//
+//  Created by Jay Versluis on 06/11/2013.
+//  Copyright (c) 2013 Pinkstone Pictures LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ ScreenSize/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  ScreenSize
+//
+//  Created by Jay Versluis on 06/11/2013.
+//  Copyright (c) 2013 Pinkstone Pictures LLC. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
